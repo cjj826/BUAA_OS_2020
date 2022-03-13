5b9c6921c10ed06e2657698bd0f2d5b4e65ef64e
Chen <2624882033@qq.com>
2022-03-13 17:00:43 +0800

    finish lab0

--- src/sh_test/hello_os.sh
+++ src/sh_test/hello_os.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #make a new dir
-echo "" > $2
-a=8
-while [ $a -le 1024 ]
-do 
-	sed -n ${a}'p' $1 >> $2
-	a=$[$a*4]
-done
+#echo "" > $2
+#a=8
+#while [ $a -le 1024 ]
+#do 
+#	sed -n ${a}'p' $1 >> $2
+#	a=$[$a*4]
+#done
 #sed -e '1p' -e '2p' -n $1 > $2
-#sed -e '8p' -e '32p' -e '128p' -e '512p' -e '1024p' -n $1 > $2
+sed -e '8p' -e '32p' -e '128p' -e '512p' -e '1024p' -n $1 > $2
