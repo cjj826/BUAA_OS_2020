519feabb896b4c37c57fd35ab8bee81c0d45dcd9
Chen <2624882033@qq.com>
2022-03-13 18:30:10 +0800

    finish-nlab0

--- dst/sh_test/hello_os.sh
+++ dst/sh_test/hello_os.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-#make a new dir
-touch $2
-sed '8p' $1 > $2
-sed '32p' $1 > $2
-sed '128p' $1 > $2
-sed '512p' $1 > $2
-sed '1024p' $1 > $2
+sed -e '8p' -e '32p' -e '128p' -e '512p' -e '1024p' -n $1 >$2
