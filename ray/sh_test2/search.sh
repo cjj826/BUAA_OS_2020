704ad2a8b394d65b22c900414eaf992695e14fc4
Chen <2624882033@qq.com>
2022-03-15 21:44:23 +0800

    finishFinal

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
-touch $3
-rm $3
-touch $3
-touch temp
-grep -n $2 $1 > temp
-awk -F: '{print $1}' temp >> $3
-rm temp
+grep -n $2 $1 | awk -F: '{print $1}' > $3
