68953ec7861a1029d7e4f828c55377f70bf2c64b
Chen <2624882033@qq.com>
2022-03-24 15:34:23 +0800

    V1

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -49,11 +49,13 @@ int readelf(u_char *binary, int size)
 
         int Nr;
 
-        Elf32_Shdr *shdr = NULL;
+        Elf32_Phdr *phdr = NULL;
+		Elf32_Phdr *phdr1 = NULL;
+		Elf32_Phdr *phdr2 = NULL;
 
-        u_char *ptr_sh_table = NULL;
-        Elf32_Half sh_entry_count;
-        Elf32_Half sh_entry_size;
+        u_char *ptr_ph_table = NULL;
+        Elf32_Half ph_entry_count;
+        Elf32_Half ph_entry_size;
 
 
         // check whether `binary` is a ELF file.
@@ -61,16 +63,52 @@ int readelf(u_char *binary, int size)
                 printf("not a standard elf format\n");
                 return 0;
         }
+		Elf32_Addr p_vaddr_l1;
+		Elf32_Addr p_vaddr_r1;
+		Elf32_Addr p_vaddr_l2;
+        Elf32_Addr p_vaddr_r2;
+		Elf32_Addr page_addr1;
+		Elf32_Addr page_addr2;
 
         // get section table addr, section header number and section header size.
-		ptr_sh_table = (u_char *) (ehdr->e_shoff + binary);//offset+binary
-		sh_entry_count = ehdr->e_shnum;
-		sh_entry_size = ehdr->e_shentsize;
+		ptr_ph_table = (u_char *) (ehdr->e_phoff + binary);//offset+binary
+		ph_entry_count = ehdr->e_phnum;
+		ph_entry_size = ehdr->e_phentsize;
         // for each section header, output section number and section addr. 
         int order = 0;
-		for (order = 0; order < sh_entry_count; order++) {
-			shdr = (Elf32_Shdr *) (ptr_sh_table + order * sh_entry_size);
-			printf("%d:0x%x\n", order, shdr->sh_addr);
+		int i, j;
+		int flag = 0;
+		for (i = 0; i < ph_entry_count; i++) {
+			for (j = 0; j < ph_entry_count; j++) {
+				if (i == j) continue;
+				phdr1 = (Elf32_Phdr *) (ptr_ph_table + i * ph_entry_size);
+				phdr2 = (Elf32_Phdr *) (ptr_ph_table + j * ph_entry_size);
+				p_vaddr_l1 = phdr1 -> p_vaddr;
+				p_vaddr_r1 = phdr1 -> p_vaddr + phdr1 -> p_memsz;
+				p_vaddr_l2 = phdr2 -> p_vaddr;
+                p_vaddr_r2 = phdr2 -> p_vaddr + phdr2 -> p_memsz;
+				page_addr1 = (p_vaddr_r1 >> 12) << 12;
+				page_addr2 = (p_vaddr_l2 >> 12) << 12;
+				if (page_addr1 == page_addr2) {
+					flag = 1;
+				}
+				if (flag == 1) {
+					if (p_vaddr_l2 < p_vaddr_r1) {
+						flag = 2;
+					}
+				}
+				
+			}
+		}
+		if (flag) {
+			if (flag == 1) printf("Overlay at page va : 0x%x\n", page_addr1);
+			else if (flag == 2) printf("Conflict at page va : 0x%x\n", page_addr1);
+		}
+		else if (flag == 0) {
+			for (order = 0; order < ph_entry_count; order++) {
+            	phdr = (Elf32_Phdr *) (ptr_ph_table + order * ph_entry_size);
+				printf("%d:0x%x,0x%x\n", order, phdr->p_filesz, phdr->p_memsz); 
+        	}
 		}
 		
 		// hint: section number starts at 0.
@@ -78,4 +116,3 @@ int readelf(u_char *binary, int size)
 
         return 0;
 }
-
