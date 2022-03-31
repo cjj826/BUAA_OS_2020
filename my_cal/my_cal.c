a2b9a62b1138390634329bc2391aa9cc9c01a9a7
Chen <2624882033@qq.com>
2022-03-31 15:31:18 +0800

    V5

--- my_cal/my_cal.c
+++ my_cal/my_cal.c
@@ -4,32 +4,23 @@ void _my_putchar(char ch);
 
 void my_cal() {
 	char c = _my_getchar();
-	int n1 = 0;
-	int n2 = 0;
+	int res[100];
+	int tot = 0;
+
+	int num = 0;
 	while (c != '\n') {
-		n1 *= 10;
-		n1 += c - '0';
+		num *= 10;
+		num = num +  c - '0';
 		c = _my_getchar();
 	}
-
-	c = _my_getchar();
-
-	while (c != '\n') {
-        n2 *= 10;
-        n2 += c - '0';
-        c = _my_getchar();
-    }
 	
-	int res = n1 + n2;
-	if (res == 0) {
-		_my_putchar('0');	
-	} else {
-		int base = 1000000000;
-		while (res / base == 0) base = base / 10;
-		while (base) {
-			_my_putchar(res / base + '0');
-			res = res % base;
-			base = base / 10;
-		}
+	while (num) {
+		res[tot++] = num % 2;
+		num = num / 2;
 	}
+	res[tot] = num;
+	int i;
+	for (i = tot; i >= 0 ; i--) {
+		_my_putchar(res[i] + '0');
+	} 
 }
