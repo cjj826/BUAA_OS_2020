bbf07ead2c76ec65f58463cf70e619ed572f7dab
Chen <2624882033@qq.com>
2022-05-08 20:42:09 +0800

    5.8V8

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -397,6 +397,14 @@ int sys_ipc_can_send(int sysno, u_int envid, u_int value, u_int srcva,
     e->env_ipc_value = value;
     e->env_status = ENV_RUNNABLE;
 	e->env_ipc_perm = perm;
-   	//if (srcva)
+   	if (srcva) {
+		p = page_lookup(curenv->env_pgdir, srcva, NULL);
+		if (p == 0) {
+			return -1;
+		}
+		if (r = page_insert(e->env_pgdir, p, e->env_ipc_dstva, perm)) {
+			return r;
+		}
+	}
 	return 0;
 }
