fbe478b63835ea6b3ca01ba51796515df1f2194a
Chen <2624882033@qq.com>
2022-05-08 23:18:52 +0800

    debug1

--- lib/sched.c
+++ lib/sched.c
@@ -97,7 +97,7 @@ void sched_yield(void)
 }
 
 */
-/*
+
 void sched_yield(void)
 {
 
@@ -144,25 +144,14 @@ void sched_yield(void)
 		env_run(e);
 	}
     //env_run(LIST_FIRST(&env_sched_list[0]));
-}*/
-int time_slices = 0;
+}
+/*int time_slices = 0;
 void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
     struct Env *e;
-    /*  hint:
-     *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
-     *     using LIST_REMOVE and LIST_INSERT_TAIL.
-     *  2. if (env_sched_list[point] is empty), point = 1 - point;
-     *     then search through `env_sched_list[point]` for a runnable env `e`, 
-     *     and set count = e->env_pri
-     *  3. count--
-     *  4. env_run()
-     *
-     *  functions or macros below may be used (not all):
-     *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
-     */
+
     if (curenv == NULL) {
         LIST_FOREACH(e, &env_sched_list[point], env_sched_link) {
             if (e->env_status == ENV_RUNNABLE) {
@@ -191,4 +180,4 @@ void sched_yield(void)
 	}
 	time_slices--;
 	env_run(curenv);
-}
+}*/
