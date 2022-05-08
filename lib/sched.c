77c53620bad25dd739a0e594475f83717527008e
Chen <2624882033@qq.com>
2022-05-08 21:23:26 +0800

    5.8V12

--- lib/sched.c
+++ lib/sched.c
@@ -97,7 +97,7 @@ void sched_yield(void)
 }
 
 */
-/*
+
 void sched_yield(void)
 {
 
@@ -142,31 +142,4 @@ void sched_yield(void)
 	count --;
     env_run(e);
     //env_run(LIST_FIRST(&env_sched_list[0]));
-}*/
-void sched_yield(void)
-{
-    //struct Env *e;
-    static struct Env *cur = NULL;	// cur也设为了静态变量，表示正在执行的进程
-    static int num = 0;// 当前正在遍历的链表
-    static int count = 0;// 当前进程剩余执行次数
-    /* 这个循环用于选出状态为RUNNABLE的进程， 我的链表也包含了状态为NOT_RUNNABLE的的进程*/
-    while(count <= 0 || (cur && cur -> env_status != ENV_RUNNABLE)) {
-        count = 0;
-        if(cur != NULL) {
-            LIST_REMOVE(cur, env_sched_link);// 把这个进程移出当前链表
-            /* ENV_FREE的就不能放在链表里了*/
-            if(cur -> env_status != ENV_FREE) 
-            	LIST_INSERT_HEAD(&env_sched_list[num^1], cur, env_sched_link);
-        }
-        if(LIST_EMPTY(&env_sched_list[num])) num ^= 1; //当前链表为空，就切换链表
-        if(LIST_EMPTY(&env_sched_list[num])) { //切换后还为空
-            //printf("\nsched.c/sched_yield:NO envs to run\n");
-            continue ;
-        }
-        cur = LIST_FIRST(&env_sched_list[num]); //取出一个进程
-        if(cur != NULL) count = cur -> env_pri;	//设置时间片
-        //printf("%x %d\n", cur, cur -> env_status);
-    }
-    -- count;		//时间片减1
-    env_run(cur);	//进程切换
 }
