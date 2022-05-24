73168a2fceaff2fbfc2db6eee2adc58807d6ffa7
Chen <2624882033@qq.com>
2022-05-24 12:50:27 +0800

    5.24

--- user/tltest.c
+++ user/tltest.c
@@ -7,7 +7,7 @@ void umain() {
     );
     *(int *) KERNBASE = 0;
     writef("My mission completed!\n");*/
-	syscall_sum(1, 2, 3, 4, 5, 6);
+//	syscall_sum(1, 2, 3, 4, 5, 6);
 	writef("come on!");
 	//printf("\n%d\n", a);
 }
