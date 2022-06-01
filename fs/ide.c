4a463dd55b3ae2fa99376f558b71035f5e352523
Chen <2624882033@qq.com>
2022-06-01 14:48:30 +0800

    debug

--- fs/ide.c
+++ fs/ide.c
@@ -22,6 +22,7 @@
 //
 // Hint: use syscalls to access device registers and buffers
 /*** exercise 5.2 ***/
+/*
 void
 ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 {
@@ -79,8 +80,8 @@ ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 //	If error occurrs during the read of the IDE disk, panic.
 //
 // Hint: use syscalls to access device registers and buffers
-/*** exercise 5.2 ***/
-void
+*//*** exercise 5.2 ***/
+/*void
 ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 {
 	// Your code here
@@ -130,4 +131,94 @@ ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
         }
         offset += 0x200;
     }
+}*/
+void
+ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
+{
+	// 0x200: the size of a sector: 512 bytes.
+	int offset_begin = secno * 0x200;
+	int offset_end = offset_begin + nsecs * 0x200;
+	int offset = 0;
+    u_int ide = 0x13000000;
+    u_int control;
+	while (offset_begin + offset < offset_end) {
+		// Your code here
+		// error occurred, then panic.
+        if (syscall_write_dev(&diskno, ide + 0x10, 4) < 0) {
+            user_panic("ide_read error 1");
+        }
+        control = offset_begin + offset;
+        if (syscall_write_dev(&control, ide, 4) < 0) {
+            user_panic("ide_read error 2");
+        }
+        control = 0;
+        if (syscall_write_dev(&control, ide + 0x20, 1) < 0) {
+            user_panic("ide_read error 3");
+        }
+        if (syscall_read_dev(&control, ide + 0x30, 4) < 0) {
+            user_panic("ide_read error 4");
+        }
+        if (control == 0) {
+            user_panic("ide_read failure");
+        }
+        if (syscall_read_dev(dst + offset, ide + 0x4000, 512) < 0) {
+            user_panic("ide_read error 5");
+        }
+        offset += 0x200;
+	}
+}
+
+
+// Overview:
+// 	write data to IDE disk.
+//
+// Parameters:
+//	diskno: disk number.
+//	secno: start sector number.
+// 	src: the source data to write into IDE disk.
+//	nsecs: the number of sectors to write.
+//
+// Post-Condition:
+//	If error occurrs during the read of the IDE disk, panic.
+//
+// Hint: use syscalls to access device registers and buffers
+/*** exercise 5.2 ***/
+void
+ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
+{
+	// Your code here
+	int offset_begin = secno * 0x200;
+	int offset_end = offset_begin + nsecs * 0x200;
+	int offset = 0;
+    u_int ide = 0x13000000;
+    u_int control;
+	// DO NOT DELETE WRITEF !!!
+	writef("diskno: %d\n", diskno);
+
+	while (offset_begin + offset < offset_end) {
+		// copy data from source array to disk buffer.
+		// if error occur, then panic.
+        if (syscall_write_dev(&diskno, ide + 0x10, 4) < 0) {
+            user_panic("ide_write error 1");
+        }
+        control = offset_begin + offset;
+        if (syscall_write_dev(&control, ide, 4) < 0) {
+            user_panic("ide_write error 2");
+        }
+        if (syscall_write_dev(src + offset, ide + 0x4000, 512) < 0) {
+            user_panic("ide_write error 3");
+        }
+        control = 1;
+        if (syscall_write_dev(&control, ide + 0x20, 1) < 0) {
+            user_panic("ide_write error 4");
+        }
+        if (syscall_read_dev(&control, ide + 0x30, 4) < 0) {
+            user_panic("ide_write error 5");
+        }
+
+        if (control == 0) {
+            user_panic("ide write failure");
+        }
+        offset += 0x200;
+	}
 }
