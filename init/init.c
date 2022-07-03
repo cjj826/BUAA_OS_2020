32f8af6b57af7d051ae26ac84d4f764148b33454
Chen <2624882033@qq.com>
2022-07-04 07:59:11 +0800

    7.4

--- init/init.c
+++ init/init.c
@@ -34,12 +34,12 @@ void mips_init() {
 	//ENV_CREATE(user_counttest);
 	//ENV_CREATE(user_teste);
 	
-	ENV_CREATE(user_mytestcreate);
-	ENV_CREATE(user_mytestexit);
-	ENV_CREATE(user_mytestcancel);
-	ENV_CREATE(user_mytestjoin);
-	ENV_CREATE(user_mytestdetach);
-	ENV_CREATE(user_mytestsem);
+	//ENV_CREATE(user_mytestcreate);
+	//ENV_CREATE(user_mytestexit);
+	//ENV_CREATE(user_mytestcancel);
+	//ENV_CREATE(user_mytestjoin);
+	//ENV_CREATE(user_mytestdetach);
+	//ENV_CREATE(user_mytestsem);
 	ENV_CREATE(user_counttest);
 	trap_init();
 	kclock_init();
